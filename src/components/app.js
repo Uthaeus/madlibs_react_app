b40b7c30770fbc1409f41e2120fbe077551b4d89
Uthaeus <romanlavery@gmail.com>
2018-04-17 20:21:27 -0600

    Added gitignore

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>My Test Application</h1>
+        <h1>Bottega Mad Libs</h1>
       </div>
     );
   }
